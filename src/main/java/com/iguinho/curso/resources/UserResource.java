90f740b2a6c0afccbfeb7adf9eb4dba857115d30
igorsep83 <igorlkz13@gmail.com>
2025-06-03 23:06:38 -0300

    User entity and resource

---
+++ src/main/java/com/iguinho/curso/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.iguinho.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iguinho.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "55555", "32132");
+		return ResponseEntity.ok().body(u);
+	}
+}
